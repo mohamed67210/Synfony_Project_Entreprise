b39496741f3cfc6f75d7114dd903b2676312d25e
mohamed67210 <mohamed_bechri@outlook.fr>
2022-12-22 10:42:26 +0100

    ajouter formulaire pour ajouter une nouvelle entreprise dna sla bdd

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Employe;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -16,6 +17,14 @@ class EmployeController extends AbstractController
         $employes = $doctrine->getRepository(Employe::class)->findBy([], ['nom' => 'ASC']);
         return $this->render('employe/index.html.twig', [
             'employes' => $employes,
+        ]);
+    }
+
+    #[Route('/employe/add', name: 'add_employe')]
+    public function add(ManagerRegistry $doctrine, Employe $employe = null, Request $request): Response
+    {
+        return $this->render('employe/show.html.twig', [
+            'employe' => $employe,
         ]);
     }
 
--- src/Controller/EntrepriseController.php
+++ src/Controller/EntrepriseController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Entreprise;
+use App\Form\EntrepriseType;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -14,10 +16,43 @@ class EntrepriseController extends AbstractController
     public function index(ManagerRegistry $doctrine): Response
     {
         // recuperer toutes les entreprises de la base de données
-        $entreprises = $doctrine->getRepository(Entreprise::class)->findAll();
+        $entreprises = $doctrine->getRepository(Entreprise::class)->findBy([],['dateCreation' => 'DESC']);
         $tableau = ['valeur1', 'valeur2', 'valeur3'];
         return $this->render('entreprise/index.html.twig', [
             'entreprises' => $entreprises,
+        ]);
+    }
+
+    #[Route('/entreprise/add', name: 'add_entreprise')]
+    public function add(ManagerRegistry $doctrine, Entreprise $entreprise = null, Request $request): Response
+    {
+        // construire un formulaire qui va se baser sur le $bulder dans entrepriseType
+        $form = $this->createForm(EntrepriseType::class, $entreprise);
+        $form->handleRequest($request);
+
+        // isValid () remplace les filter input 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // recuperer les données inserer dans le formilaire et les injecter dans l'objet entreprise grace au seter
+            $entreprise = $form->getData();
+
+            // on recupere le managere doctrine
+            $entityManager = $doctrine->getManager();
+
+            // persist remplace prepare en pdo , on prepare l'objet entreprise 
+            $entityManager->persist($entreprise);
+
+            // execute,inserer les données dans la bdd
+            $entityManager->flush();
+
+            // retourner a la page qui affiche toutes les entreprises
+
+            return $this->redirectToRoute('app_entreprise');
+        }
+
+        // vue pour afficher le formulare d'ajout
+        return $this->render('entreprise/add.html.twig', [
+            'formAddEntreprise' => $form->createView(),
         ]);
     }
 
